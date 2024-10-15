e912f5e1fa7aa30bdd38d76c89286cd026c990d0
Nhlalo <nhlalonkosi@gmail.com>
2024-10-15 12:49:48 +0200

    Add index.html to the dev server's array of watched files
    This wil make webpack-dev-serve detect changes to our html file.

--- webpack.common.js
+++ webpack.common.js
@@ -4,14 +4,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/index.js"
-},
+    app: "./src/index.js",
+  },
   output: {
     filename: "main.bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
- 
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
